bc60640ef9260fea86c8aea599f39f7bf65e31b7
wang zuo <wzuoadjusted@gmail.com>
2015-08-03 23:37:33 +0800

    path fix

--- cli.js
+++ cli.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-var path = require('path');
 var locales = require('./');
 var argv = process.argv;
-
-locales(
-  path.join(__dirname, argv[2]),
-  path.join(__dirname, argv[3])
-);
+locales(argv[2], argv[3]);
