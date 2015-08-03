541d6525cd336bb2d79da5472cea08da952a448f
wang zuo <wzuoadjusted@gmail.com>
2015-08-03 23:42:39 +0800

    cli.js with mkdirp

--- cli.js
+++ cli.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
 var locales = require('./');
+var mkdirp = require('mkdirp');
 var argv = process.argv;
-locales(argv[2], argv[3]);
+
+var lst = argv[2];
+var dir = argv[3] || 'locales';
+
+mkdirp(dir, function() {
+  locales(lst, dir);
+});
